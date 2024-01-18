d64f2f19a8e0e738aed9e36aa3006528f603e063
annelis1 <119529206+annelis1@users.noreply.github.com>
2024-01-17 21:21:25 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Assign2;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Time for the Dice Throwing Simulator!");
+        Console.WriteLine("How many dice rolls would you like to do?");
+        
+        //what is this and why does it work? another chat thing
+        int.TryParse(Console.ReadLine(), out int rolls);
+            //console.readLine() treats it as a string
+            //int.TryParse(..) method to string--> int
+            //out int rolls declares output named rolls
+        
+        //taking userinput and sending to result class
+        results Results = new results();
+        int[] diArray = Results.MakeHist(rolls);
+
+        Console.WriteLine("Rolling " + rolls + " times complete");
+        //print histogram that shows % each number was rolled
+        // Each * represents 1% of total rolls
+        
+        //send array and rolls to the function
+        PrintHist(diArray, rolls);
+
+        Console.WriteLine("Sayonara suckers!");
+    }
+    static void PrintHist(int[] diArray, int rolls)
+    {
+        for (int i = 0; i < diArray.Length; i++) 
+        {
+            // frequency of each number
+            int percent = diArray[i]* 100 / rolls;
+            
+            // creates string which does * for value stored 
+            string histogram = new string('*', percent);
+            // new string() uses the string constructor
+            // * is character to be repeated percent variable amount of times
+
+            //alt way- for loop to do number given from percent print 1 *
+            
+            // print '*' proportional to percent
+            Console.WriteLine($"{i + 2}: {histogram} ({percent}%)");
+            
+        }
+    }
+}
---
+++ results.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign2
+{
+    public class results
+    {
+        public int[] MakeHist(int rolls)
+        {
+            // random number generator class
+            Random random = new Random();
+            int[] diArray = new int[11];//created array for results to be stored
+
+            // loops through the user inputed rolls
+            for (int i = 0; i < rolls; i++)
+            {
+          //simulates the roll of the dice for the number of times user specified
+                int die1 = random.Next(1, 7);
+                           // max value(7) is not included but min value(1) is
+                int die2 = random.Next(1, 7);
+                int total = die1 + die2;
+             
+             //adding results for each roll total to the array
+                diArray[total - 2]++;
+                // 0-10 in arrays but totals are 2-12 so have to do -2  
+            }
+            
+            return diArray;
+          
+        }
+    }
+}
